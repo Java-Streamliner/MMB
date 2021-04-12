759d3138760628315ab33a05504a410bbb9f8045
Tiger <thisisstreamliner@gmail.com>
2021-04-12 18:05:28 +0300

    Add Pictures. Create Register Activity Layout

---
+++ app/src/main/java/com/thisisstreamliner/mmb/authentication/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.thisisstreamliner.mmb.authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.thisisstreamliner.mmb.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
